5113b7e4071130c4d8c7fd78870f3d944558d293
Adrian Ismu Arifianto <71255346+adrianismu@users.noreply.github.com>
2023-10-15 02:13:28 +0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Dark_Admin_Panel
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Item_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CartesianChart_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void InfoCard_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
